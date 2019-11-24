b577b7b3b96b1affa31626c743dfb47d0608a05b
Srinjoy Chakravarty <sc7839@nyu.edu>
2019-11-24 02:49:16 -0500

    Identify the most commonly occuring single or double proper nouns

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
-from tika import parser
-raw = parser.from_file('1philosophersstone.pdf')
-#print(raw['content'])
-
-from pdfminer.pdfparser import PDFParser
-from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdfpage import PDFPage
-from pdfminer.pdfinterp import resolve1
-import os
+import nltk, os
+from io import StringIO
+from nltk import pos_tag, word_tokenize
 from os import listdir
 from os.path import isfile, join
+from pdfminer.converter import TextConverter
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import resolve1
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+from tika import parser
+from collections import Counter
 
-from nltk import word_tokenize
-
-
-def count_pages():
+def count_pages(path):
     '''
     Count the number of pages in the book
     '''
     page_count_dict = {}
-    mypath = os.getcwd()
     for file in os.listdir(mypath):
         if file.endswith(".pdf"):
             with open(file, 'rb') as f:
@@ -27,7 +24,6 @@ def count_pages():
                 page_count_dict[file] = resolve1(book.catalog['Pages'])['Count']
             f.close()
     return page_count_dict
-
 
 def read_text():
     '''
@@ -41,7 +37,6 @@ def read_text():
                 parser = PDFParser(f)
                 book = PDFDocument(parser)
                 all_books_text.append(book)
-                print(file, resolve1(book.catalog['Pages'])['Count'])
             f.close()
     return all_books_text
 
@@ -57,27 +52,69 @@ def text_tokenize(book):
     '''
     tokenize = word_tokenize(book)
     return tokenize
-# mypath = os.getcwd()
-# for file in os.listdir(mypath):
-#     if file.endswith(".pdf"):
-#         opened_file = open(file, 'rb')
-#         parser = PDFParser(opened_file)
-#         document = PDFDocument(parser)
-#         # This will give you the count of pages
-#         print(file, resolve1(document.catalog['Pages'])['Count'])
 
+def tagging(tokenize):
+    '''
+    This function takes the tokenized text created by the text_tokenize function
+    and tags each word with a code for the part of speech it represents
+    using NLTK's algorithm.  So, it changes the tokenized output:
+    ['Crabbe', 'hung', 'back', 'for', 'a', 'last', 'word',
+    'with', 'Tom', 'and', 'Goyle', '.']
+    - TO -
+    [('Crabbe', 'NNP'),
+    ('hung', 'VBD'), ('back', 'RP'), ('for', 'IN'), ('a', 'DT'), ('last', 'JJ'),
+    ('word', 'NN'), ('with', 'IN'), ('Tom', 'NNP'), ('and', 'CC'),
+    ('Goyle', 'NNP'), ('.', '.')]
+    '''
+    tagged_text = pos_tag(tokenize)
+    return tagged_text
 
-# import nltk
-# import nltk.data
-# nltk.download('punkt')
-# tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
-#
-# corpus = nltk.corpus.reader.PlaintextCorpusReader("1philosophersstone.pdf", r'.*', encoding='latin-1')
-#fp = open("1philosophersstone.pdf")
-#fp = open('1philosophersstone.pdf', encoding = "utf-8")
-#job_titles = [line.decode('utf-8').strip() for line in fp.readlines()]
-#data = corpus.read()
-# print('\n-----\n'.join(tokenizer.tokenize(str(corpus))))
+def find_proper_nouns(tagged_text):
+    '''
+    This function takes in the tagged text from the tagging function and returns
+    a list of words that were tagged as proper nouns.  It does this by looking
+    at the second value in each word/tag pair - e.g. ('Lucius', 'NNP') and determining
+    if is is equal to 'NNP'.
+    There are a lot of characters in these novels who are referred to with two
+    proper nouns, like 'Professor Quirell', 'Mrs. Malfoy', or 'Uncle Vernon',
+    and any character can be called by their full name (e.g. 'Salazar Slytherin').
+    So, if the second value IS equal to 'NNP', we check the second value of the
+    next word - if it is also equal to 'NNP', we string the two words together
+    and add them to the proper_nouns list.
+    If the second value ISN'T equal to 'NNP', we append (add) only the first
+    word to the proper_nouns list.
+    As we add nouns to the list, we put them all in lower case - otherwise, our
+    program won't know that 'HARRY' is the same thing for our purposes as 'Harry'.
+    '''
+    proper_nouns = []
+    i = 0
+    while i < len(tagged_text):
+        if tagged_text[i][1] == 'NNP':
+            if tagged_text[i+1][1] == 'NNP':
+                proper_nouns.append(tagged_text[i][0].lower() +
+                                    " " + tagged_text[i+1][0].lower())
+                i+=1 # extra increment added to the i counter to skip the next word
+            else:
+                proper_nouns.append(tagged_text[i][0].lower())
+        i+=1 # increment the i counter
+    return (proper_nouns)
+
+def summarize_text(proper_nouns, top_num):
+    '''
+    This function takes the proper_nouns from the list created by the
+    find_proper_nouns function and counts the instances of each using
+    the most_common method that comes with the counter.
+    '''
+    counts = dict(Counter(proper_nouns).most_common(top_num))
+    return counts
+
 if __name__== "__main__":
-    print(count_pages())
-    #print(type(read_text()[2]))
+    mypath = os.getcwd()
+    print(count_pages(mypath))
+    raw = parser.from_file('6halfbloodprince.pdf')
+    tokenized_half_blood_prince = text_tokenize(raw['content'])
+    nltk.download('averaged_perceptron_tagger')
+    tagged_half_blood_prince = tagging(tokenized_half_blood_prince)
+    proper_nouns_half_blood_prince = find_proper_nouns(tagged_half_blood_prince)
+    top_thirty_proper_nouns_half_blood_prince = summarize_text(proper_nouns_half_blood_prince, 30)
+    print(top_thirty_proper_nouns_half_blood_prince)
